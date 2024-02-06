e0094f4589a81058a2433095eef31f40349187ca
ethiohacking301 <ethiohacking301@gmail.com>
2024-02-06 14:57:03 +0300

    update root to accept wireless inteface

--- cmd/root.go
+++ cmd/root.go
@@ -3,13 +3,21 @@ package cmd
 import (
 	"os"
 
+	"github.com/hacker301et/wifipen/cmd/logic"
 	"github.com/spf13/cobra"
 )
 
+var iface string
+
 var rootCmd = &cobra.Command{
 	Use:   "wifipen",
-	Short: "wifipen is wifi penetration testing tool",
+	Short: "",
 	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		w := logic.Init(iface)
+		w.Start()
+	},
 }
 
 func Execute() {
@@ -17,8 +25,14 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+
 }
 
 func init() {
+
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&iface, "iface", "i", "", "wifi inteface used to creack wifi")
+	if err := rootCmd.MarkFlagRequired("iface"); err != nil {
+		return
+	}
 }
